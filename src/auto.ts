d67a0d7c98fa5988f1547125531d3a62782072fd
Evan King <e.king@utexas.edu>
2025-01-14 11:33:08 -0600

    Update CDN asset paths

--- src/auto.ts
+++ src/auto.ts
@@ -3,7 +3,7 @@ import { MoonshineSettings } from "./constants";
 
 // set the asset path to the CDN root since this script version is intended for CDN use only
 MoonshineSettings.BASE_ASSET_PATH =
-    "https://cdn.jsdelivr.net/npm/@usefulsensors/moonshine@latest/dist/";
+    "https://cdn.jsdelivr.net/npm/@usefulsensors/moonshine-js@latest/dist/";
 
 var elementManager = new MoonshineElementManager("model/tiny");
 elementManager.autoInjectElements();
