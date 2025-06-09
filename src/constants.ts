168f667a828dc97ef1697209618955c9d1102498
Evan King <e.king@utexas.edu>
2025-06-09 13:25:08 -0600

    Add ability to change all asset paths

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,10 @@ export const Settings = {
     FRAME_SIZE: 250,
     MAX_SPEECH_SECS: 5,
     MAX_RECORD_MS: 60000,
-    // BASE_ASSET_PATH: "/dist/"
-    BASE_ASSET_PATH: "https://cdn.jsdelivr.net/npm/@usefulsensors/moonshine-js@latest/dist/",
+    BASE_ASSET_PATH: {
+        MOONSHINE: "https://cdn.jsdelivr.net/npm/@usefulsensors/moonshine-js@latest/dist/",
+        ONNX_RUNTIME: "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.22.0/dist/",
+        SILERO_VAD: "https://cdn.jsdelivr.net/npm/@ricky0123/vad-web@latest/dist/"
+    },
     VERBOSE_LOGGING: false
 }
