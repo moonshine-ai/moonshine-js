d9a668d7d497d2355758e01bc2e99e863b57ed17
Evan King <e.king@utexas.edu>
2025-06-19 17:38:31 -0700

    Add video to quickstart example

--- examples/quickstart/index.js
+++ examples/quickstart/index.js
@@ -1,53 +1,74 @@
 import * as Moonshine from "https://cdn.jsdelivr.net/npm/@usefulsensors/moonshine-js@latest/dist/moonshine.min.js"
+// uncomment to use latest build in this repo
+// import * as Moonshine from "@usefulsensors/moonshine-js"
 
 const state = document.getElementById("state")
 const button = document.getElementById("button")
+const history = document.getElementById("history")
+var pastText = ""
 
-var transcriber = new Moonshine.MicrophoneTranscriber(
-    "model/tiny",
-    {
-        onPermissionsRequested() {
-            state.innerHTML = "Requesting permissions."
-        },
-        onError(e) {
-            if (e == Moonshine.MoonshineError.PermissionDenied) {
-                state.innerHTML = "Permission denied."
-            }
-        },
-        onModelLoadStarted() {
-            state.innerHTML = "Model loading."
-        },
-        onModelLoaded() {
-            state.innerHTML = "Model loaded."
-        },
-        onTranscribeStarted() {
-            state.innerHTML = "Started. Waiting for speech."
-        },
-        onSpeechStart() {
-            state.innerHTML = "Speech started. Listening."
-        },
-        onSpeechEnd() {
-            state.innerHTML = "Speech ended."
-        },
-        onTranscriptionUpdated(text) {
-            if (text) {
-                state.innerHTML = "Transcript: " + text
-            }
-        },
-        onTranscribeStopped() {
-            state.innerHTML = "Stopped."
+const callbacks = {
+    onPermissionsRequested() {
+        state.innerHTML = "Requesting permissions."
+    },
+    onError(e) {
+        if (e == Moonshine.MoonshineError.PermissionDenied) {
+            state.innerHTML = "Permission denied."
         }
     },
+    onModelLoadStarted() {
+        state.innerHTML = "Model loading."
+    },
+    onModelLoaded() {
+        state.innerHTML = "Model loaded."
+    },
+    onTranscribeStarted() {
+        state.innerHTML = "Started. Waiting for speech."
+    },
+    onSpeechStart() {
+        state.innerHTML = "Speech started. Listening."
+    },
+    onSpeechEnd() {
+        state.innerHTML = "Speech ended."
+    },
+    onTranscriptionCommitted(text) {
+        if (text) {
+            state.innerHTML = "Transcript committed."
+            pastText = `${pastText} <br> ${text}`
+            history.innerHTML = pastText
+        }
+    },
+    onTranscriptionUpdated(text) {
+        if (text) {
+            state.innerHTML = "Transcript updated."
+            history.innerHTML = `${pastText} <br> ${text}`
+        }
+    },
+    onTranscribeStopped() {
+        state.innerHTML = "Stopped."
+    }
+}
+
+var microphoneTranscriber = new Moonshine.MicrophoneTranscriber(
+    "model/tiny",
+    callbacks,
+    false
+);
+
+var videoTranscriber = new Moonshine.MediaElementTranscriber(
+    document.getElementById("video"),
+    "model/tiny",
+    callbacks,
     false // use streaming mode, rather than VAD chunks
 );
 
 button.addEventListener("click", () => {
-    if (transcriber.isActive) {
-        transcriber.stop()
+    if (microphoneTranscriber.isActive) {
+        microphoneTranscriber.stop()
         button.innerText = "Start"
     }
     else {
-        transcriber.start()
+        microphoneTranscriber.start()
         button.innerText = "Stop"
     }
 })
