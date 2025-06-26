9a997ce1706a1ff2c66f32e335142706ec0ffe33
Pete Warden <pete@usefulsensors.com>
2025-06-25 20:48:33 -0700

    Added minimal video caption example

---
+++ examples/video-captions/index.js
@@ -0,0 +1,9 @@
+// import * as Moonshine from "https://cdn.jsdelivr.net/npm/@moonshine-ai/moonshine-js@latest/dist/moonshine.min.js"
+// uncomment to use latest build in this repo
+import * as Moonshine from "@moonshine-ai/moonshine-js"
+
+var video = document.getElementById("video");
+var videoCaptioner = new Moonshine.VideoCaptioner(video, "model/base", false);
+video.addEventListener("play", () => {
+    videoCaptioner.start();
+});
